a48b96e7b5bb9bf125a90e49cb20f1b9bd6a66a6
Esarndev <dev@esarn.com>
2017-07-11 16:45:40 +0700

    update1.1 cp

---
+++ car-list.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title></title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/smoothness/bootstrap.min.css">
+		<script src="/js/jquery-3.2.1.min.js"></script>
+		<script src="/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="css/smoothness/carlist-styte.css">
+		<link rel="stylesheet" type="text/css" href="css/smoothness/jquery-ui-1.7.2.custom.css">
+		<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+		<script type="text/javascript" src="js/jquery-ui-1.7.2.custom.min.js"></script>
+		<script type="text/javascript">
+			$(function(){
+    
+    		$("#tabs").tabs(); 
+ 
+		});
+		</script>
+	</head>
+	<body>
+		<nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header row">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <div>
+                        <a class="navbar-brand" href="">รถถูก.com</a>
+                    </div>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a>สมัครสมาชิก</a></li>
+                        <li><a >เข้าสู่ระบบ</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+<div class="boximg">
+	<div class="container-fluid row">
+		<div class ="box">
+		<div class="col-md-5 text-right tex">
+			 
+				<div class="boximg">
+					<p class="groove">รูปโปร์ไฟล์</p>
+				</div>
+				</div>	
+						
+		<div class="col-md-7 text-left tex">
+			<div class="btn btn-info btn">
+				<p>img</p>
+			</div>	
+		</div>
+
+	</div>
+</div> 
+	
+<div class="col-md-12">
+    <div id="tabs">
+    <ul>
+        <li><a href="#tabs-1">Car list</a></li>
+        <li><a href="#tabs-2">Sell list</a></li>
+        <li><a href="#tabs-3">Sell</a></li>
+        <li><a href="#tabs-4">Profile detal</a></li>
+    </ul>
+    <div id="tabs-1">
+        <p>รายละเอียดย่อยหัวข้อแท็บที่ 1</p>
+    </div>
+    <div id="tabs-2">
+        <p>รายละเอียดย่อยหัวข้อแท็บที่ 2</p>
+    </div>
+    <div id="tabs-3">
+        <p>รายละเอียดย่อยหัวข้อแท็บที่ 3</p>
+    </div>
+    <div id="tabs-4">
+        <p>รายละเอียดย่อยหัวข้อแท็บที่ 4</p>
+    </div>
+</div>
+</div>
+
+
+
+
+
+
+	</body>
+</html>
