6b85bfc5eb029cc4f6952c50f050da34ce27e062
Esarndev <dev@esarn.com>
2017-07-12 18:42:42 +0700

    detail v.1 html

---
+++ detail.php
@@ -0,0 +1,217 @@
+<html lang="Th-th">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/detail.css">
+    <link rel="stylesheet" href="css/MainTheme.css">
+</head>
+<style>
+.mySlides {display:none}
+.demo {cursor:pointer}
+</style>
+<body>
+  <!--- header --->
+    <div class="panel panel-default">
+            <div class="panel-heading">
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <div class="navbar-header row">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menuNavbar">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <div>
+                                <a class="navbar-brand" href="">รถถูก.com</a>
+                            </div>
+                        </div>
+                        <div class="collapse navbar-collapse" id="menuNavbar">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a>สมัครสมาชิก</a></li>
+                                <li><a>เข้าสู่ระบบ</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+    <!--- header --->
+    <!--- minor --->
+    <!-- <div class="navbar-fixed-top">
+        <div class="container-fluid">
+          <div class="col-sm-1"></div>
+          <div class="col-sm-7">
+            <p class="textmenu">Test</p>
+          </div>
+          <div class="col-sm-4"></div>
+        </div>
+    </div> -->
+    <!--- Content --->
+        <!-- Head Content -->
+        <div class="container-fluid boxcontenthead">
+          <div class="col-sm-5">
+            <div>
+              <h5><strong>2008 Proton Gen 2 1.6</strong></h5>
+              <p style="font-size: 14px;" class="visible-lg">(ปี 07-12) Medium Line Sedan AT</p>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="textheadcontent text-left">
+              <h4><strong style="color:red;">149,000 บาท</strong></h4>
+            </div>
+          </div>
+          <div class="col-sm-2 text-right visible-lg">
+            <div class="textheadcontent">
+              <h2><strong><a href="#"><i class="material-icons" style="color:red;">favorite</i></a></strong></h2>
+            </div>
+          </div>
+          <div class="col-sm-2 text-left visible-lg">
+            <div class="textheadcontent">
+              <h5>เปรียบเทียบ</h5>
+            </div>
+          </div>
+        </div>
+        <!-- Head Content -->
+        <!--- Slideshow Content -->
+    <div class="container-fluid boxcontentslide">
+      <div class="w3-content" style="max-width:483px">
+          <img class="mySlides" src="http://www.mx7.com/i/223/ROCaU4.jpg" style="width:100%">
+          <img class="mySlides" src="http://www.mx7.com/i/04d/89qkER.jpg" style="width:100%">
+          <img class="mySlides" src="http://www.mx7.com/i/29a/hYfHex.jpg" style="width:100%">
+          <img class="mySlides" src="http://www.mx7.com/i/1fe/itwTBz.jpg" style="width:100%">
+          <img class="mySlides" src="http://www.mx7.com/i/1f8/siib0S.jpg" style="width:100%">
+          <img class="mySlides" src="http://www.mx7.com/i/0c5/lBzoQx.jpg" style="width:100%">
+
+          <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+          <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
+        </div>
+        <div class="w3-center">
+          <div class="w3-section">
+          <div class="w3-row-padding w3-section visible-lg">
+            <div class="w3-col s2">
+              <img class="demo w3-opacity w3-hover-opacity-off" src="http://www.mx7.com/i/223/ROCaU4.jpg" style="width:75%" onclick="currentDiv(1)">
+            </div>
+            <div class="w3-col s2">
+              <img class="demo w3-opacity w3-hover-opacity-off" src="http://www.mx7.com/i/04d/89qkER.jpg" style="width:75%" onclick="currentDiv(2)">
+            </div>
+            <div class="w3-col s2">
+              <img class="demo w3-opacity w3-hover-opacity-off" src="http://www.mx7.com/i/29a/hYfHex.jpg" style="width:75%" onclick="currentDiv(3)">
+            </div>
+            <div class="w3-col s2">
+              <img class="demo w3-opacity w3-hover-opacity-off" src="http://www.mx7.com/i/1fe/itwTBz.jpg" style="width:75%" onclick="currentDiv(4)">
+            </div>
+            <div class="w3-col s2">
+              <img class="demo w3-opacity w3-hover-opacity-off" src="http://www.mx7.com/i/1f8/siib0S.jpg" style="width:75%" onclick="currentDiv(5)">
+            </div>
+            <div class="w3-col s2">
+              <img class="demo w3-opacity w3-hover-opacity-off" src="http://www.mx7.com/i/0c5/lBzoQx.jpg" style="width:75%" onclick="currentDiv(6)">
+            </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    <script>
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+      showDivs(slideIndex += n);
+    }
+
+    function currentDiv(n) {
+      showDivs(slideIndex = n);
+    }
+
+    function showDivs(n) {
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("demo");
+      if (n > x.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = x.length}
+      for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+         dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+      }
+      x[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " w3-opacity-off";
+    }
+    </script>
+      <!--- Slideshow Content -->
+      <!--- Comment --->
+      <div class="container-fluid comment">
+        <div>
+          <h5><strong>รายละเอียดพอสังเขป</strong></h5>
+        </div>
+          <div class="incomment">
+            <p>ขายรถบ้าน Proton Gen2 1.6 ปี2008 เครื่องเดิมๆ เบาะผ้าโรงงาน ภายในสภาพเดิมไม่ดัดแปลง วิ่งไป 150k กว่าๆ
+               ติดแก๊ส LPG ราคา 149,000 บาท **สนใจติดต่อ แสดงหมายเลขโทรศัพท์ 0900175982 (เน็ก)
+หรือนัดดูรถได้แถวมหิดลศาลายา**</p>
+          </div>
+      </div>
+      <!--- Comment --->
+      <!--- Main Content --->
+      <div class="container-fluid maincontent">
+          <div class="w3-bar w3-black">
+            <button class="w3-bar-item w3-button tablink w3-red" onclick="openmain(event,'main')">ข้อมูลรถ</button>
+            <button class="w3-bar-item w3-button tablink" onclick="openmain(event,'pro')">โปรโมชั่น</button>
+          </div>
+          <div id="main" class="w3-container w3-border maindetail detail">
+                <h5><strong>สเปครถ</strong></h5>
+                  <div class="newspaper">
+                    <p>ประเภทรถ : <strong>รถมือสอง</strong></p>
+                    <p>ยี่ห้อ : <strong>Proton</strong></p>
+                    <p>รุ่น : <strong>Gen 2</strong></p>
+                    <p>โฉมรถยนต์ : <strong>(ปี 07-12)</strong></p>
+                    <p>รายละเอียดรุ่น : <strong>Medium Line</strong></p>
+                    <p>ปี : <strong>2008</strong></p>
+                    <p>ขนาดเครื่องยนต์ : <strong>1597 ซีซี</strong></p>
+                    <p>ระบบเกียร์ : <strong>เกียร์อัตโนมัติ</strong></p>
+                    <p>เลขไมล์ (กม.) : <strong>150 - 155K</strong></p>
+                    <p>สี : <strong>สีขาว</strong></p>
+                  </div><hr>
+                  <h5><strong>ที่อยู่</strong></h5>
+                    <div class="newspaper">
+                      <p><strong>ภาคกลาง</strong></p>
+                  </div><hr>
+            </div>
+          <div id="pro" class="w3-container w3-border maindetail detail" style="display:none">
+            <h5><strong>โปรโมชั่น</strong></h5>
+              <div class="newspaper">
+                <p><strong>ยังไม่โปรโมชั่นในขณะนี้</strong></p>
+              </div><hr>
+          </div>
+      <script>
+        function openmain(evt, detailname) {
+        var i, x, tablinks;
+        x = document.getElementsByClassName("detail");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablink");
+        for (i = 0; i < x.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+        }
+        document.getElementById(detailname).style.display = "block";
+        evt.currentTarget.className += " w3-red";
+        }
+      </script>
+      <!--- Main Content --->
+      <!--- Footer --->
+        <div class="panel-footer">
+            <div class="container-fluid">
+                   <ul class="foot-link">
+                       <li><a>เกี่ยวกับเรา</a></li>
+                       <li><a>ติดต่อเรา</a></li>
+                   </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
